eabe658f0092b3c7026c242150b277e6e1c1f696
tomer-haba <tomer.haba@post.runi.ac.il>
2025-11-21 14:25:53 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -1,4 +1,23 @@
 public class Algebra {
+	public static void main(String args[]) {
+	    // Tests some of the operations
+	    System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(minus(7,2));  // 7 - 2
+   		System.out.println(minus(2,7));  // 2 - 7
+ 		System.out.println(times(3,4));  // 3 * 4
+   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		System.out.println(pow(5,3));      // 5^3
+   		System.out.println(pow(3,5));      // 3^5
+   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(5,5));    // 5 / 5  
+   		System.out.println(div(25,7));   // 25 / 7
+   		System.out.println(mod(25,7));   // 25 % 7
+   		System.out.println(mod(120,6));  // 120 % 6    
+   		System.out.println(sqrt(36));
+		System.out.println(sqrt(263169));
+   		System.out.println(sqrt(76123));
+	}  
+public class Algebra {
 
     private static int internalAbs(int a) {
         if (a >= 0) {
