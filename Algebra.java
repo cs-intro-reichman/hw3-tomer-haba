11a3816fac9997036c0840f58efdfb87ace64511
tomer-haba <tomer.haba@post.runi.ac.il>
2025-11-21 14:13:03 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -1,115 +1,124 @@
-// Implements algebraic operations and the square root function without using 
-// the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
-// Math.sqrt. All the functions in this class operate on int values and
-// return int values.
-
 public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
 
-// Returns x1 + x2
-public static int plus(int x1, int x2) {
-// Replace the following statement with your code
-int result = 0;
-result = x1;
+    private static int internalAbs(int a) {
+        if (a >= 0) {
+            return a;
+        int result = 0;
+        int current = a;
+        while (current < 0) {
+            current++; 
+            result++; 
+        }
+        return result;
+    }
 
-for(int i = 0; i<x2; i++){
-result++;
-}
+    public static void main(String args[]) {
+   
+    }  
 
-return result;
-}
+    public static int plus(int x1, int x2) {
+        int absX2 = internalAbs(x2);
+        boolean isNegative = x2 < 0;
 
-// Returns x1 - x2
-public static int minus(int x1, int x2) {
-// Replace the following statement with your code
-int result = 0;
-result = x1;
+        for(int i = 0; i < absX2; i++) {
+            if (isNegative) {
+                x1--; 
+            } else {
+                x1++; 
+            }
+        }
+        return x1;
+    }
 
-for(int i = 0; i<x2; i++){
-result--;
-}
+    public static int minus(int x1, int x2) {
+        int absX2 = internalAbs(x2);
+        boolean isNegative = x2 < 0;
 
-return result;
-}
+        for(int i = 0; i < absX2; i++) {
+            if (isNegative) { 
+                x1++; 
+            } else { 
+                x1--; 
+            }
+        }
+        return x1;
+    }
 
-// Returns x1 * x2
-public static int times(int x1, int x2) {
-// Replace the following statement with your code
-int result = 0;
-result = x1;
 
-for(int i = 0; i<x2; i++){
-result = plus(result,x1);
-}
+    public static int times(int x1, int x2) {
+        if (x1 == 0 || x2 == 0) return 0;
+        
+        int result = 0;
+        int absX1 = internalAbs(x1);
+        int absX2 = internalAbs(x2);
+        
+        boolean isNegativeResult = (x1 < 0 && x2 >= 0) || (x1 >= 0 && x2 < 0);
+        
+        for(int i = 0; i < absX2; i++) { 
+            result = plus(result, absX1);
+        }
+        
+        if (isNegativeResult) {
+            return minus(0, result);
+        } else {
+            return result;
+        }
+    }
+    public static int pow(int x, int n) {
+        if (n < 0) return 0; 
+        if (n == 0) return 1; 
 
-return result;
-}
+        int result = 1; 
+        
+        for(int i = 0; i < n; i++) {
+            result = times(result, x); 
+        }
+        return result;
+    }
+    
+    public static int div(int x1, int x2) {
+        if (x2 == 0) return 0;
+        
+        int result = 0;
+        
+        int absX1 = internalAbs(x1);
+        int absX2 = internalAbs(x2);
 
-// Returns x^n (for n >= 0)
-public static int pow(int x, int n) {
-// Replace the following statement with your code
-int result = 0;
-result = x;
+        boolean isNegativeResult = (x1 < 0 && x2 >= 0) || (x1 >= 0 && x2 < 0);
+        
+        while (absX1 >= absX2) {
+            absX1 = minus(absX1, absX2); 
+            result = plus(result, 1);       
+        }
+        
+        if (isNegativeResult) {
+            return minus(0, result); 
+        } else {
+            return result;
+        }
+    }
 
-for(int i = 0; i<n; i++){
-result = times(result,x);
-}
+    public static int mod(int x1, int x2) {
+        if(x2 == 0) return 0;
+        
+        int quotient = div(x1, x2);
+        int product = times(quotient, x2);
+        int remainder = minus(x1, product);
+        
+        return remainder;
+    }   
+    public static int sqrt(int x) 
+    {
+        if (x < 0) return 0;
+        if (x == 0) return 0;
 
-return result;
-}
+        int g = 1;
+        
+        while (times(g, g) <= x) 
+        { 
+            g = plus(g, 1); 
+        }
 
-// Returns the integer part of x1 / x2
-public static int div(int x1, int x2) {
-// Replace the following statement with your code
-int result = 0;
-result = x1;
-
-for(int i = 0; i<x2; i++){
-result = minus(result,x1);
-}
-
-return result;
-}
-
-// Returns x1 % x2
-public static int mod(int x1, int x2) {
-// Replace the following statement with your code
-int counter = 0;
-
-while(counter <= x1){
-counter = plus(counter,x2);
-}
-
-counter = minus(counter,x2);
-
-int mod = minus(x1,counter);
-
-return mod;
-}
-
-// Returns the integer part of sqrt(x)
-public static int sqrt(int x) {
-// Replace the following statement with your code
-for(int i = 1; i<=x; i++){
-if (x/i == i){return i;}
-}
-return 0;
-}
+        return minus(g, 1); 
+    }
 }
