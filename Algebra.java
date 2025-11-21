bf21ce774def7602f3b5b1de3192d339597cb426
tomer-haba <tomer.haba@post.runi.ac.il>
2025-11-21 09:08:55 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -23,45 +23,93 @@ public class Algebra {
    		System.out.println(sqrt(76123));
 	}  
 
-	// Returns x1 + x2
-	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+// Returns x1 + x2
+public static int plus(int x1, int x2) {
+// Replace the following statement with your code
+int result = 0;
+result = x1;
 
-	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+for(int i = 0; i<x2; i++){
+result++;
+}
 
-	// Returns x1 * x2
-	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+return result;
+}
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+// Returns x1 - x2
+public static int minus(int x1, int x2) {
+// Replace the following statement with your code
+int result = 0;
+result = x1;
 
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+for(int i = 0; i<x2; i++){
+result--;
+}
 
-	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+return result;
+}
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+// Returns x1 * x2
+public static int times(int x1, int x2) {
+// Replace the following statement with your code
+int result = 0;
+result = x1;
+
+for(int i = 0; i<x2; i++){
+result = plus(result,x1);
+}
+
+return result;
+}
+
+// Returns x^n (for n >= 0)
+public static int pow(int x, int n) {
+// Replace the following statement with your code
+int result = 0;
+result = x;
+
+for(int i = 0; i<n; i++){
+result = times(result,x);
+}
+
+return result;
+}
+
+// Returns the integer part of x1 / x2
+public static int div(int x1, int x2) {
+// Replace the following statement with your code
+int result = 0;
+result = x1;
+
+for(int i = 0; i<x2; i++){
+result = minus(result,x1);
+}
+
+return result;
+}
+
+// Returns x1 % x2
+public static int mod(int x1, int x2) {
+// Replace the following statement with your code
+int counter = 0;
+
+while(counter <= x1){
+counter = plus(counter,x2);
+}
+
+counter = minus(counter,x2);
+
+int mod = minus(x1,counter);
+
+return mod;
+}
+
+// Returns the integer part of sqrt(x)
+public static int sqrt(int x) {
+// Replace the following statement with your code
+for(int i = 1; i<=x; i++){
+if (x/i == i){return i;}
+}
+return 0;
+}
 }
